2f2d032cf4f3fa2184ceb587520a0b1e8b4d0b3a
BIGANNOY <xm1294395667@outlook.com>
2023-09-14 23:53:03 +0800

    更改

--- dataGetter/views.py
+++ dataGetter/views.py
@@ -9,27 +9,32 @@ from datetime import timedelta
 from django.views.decorators.http import require_http_methods
 from tabulate import tabulate
 import warnings
+
 warnings.filterwarnings("ignore")
-#显示所有列
+# 显示所有列
 pd.set_option('display.max_columns', None)
-#显示所有行
+# 显示所有行
 pd.set_option('display.max_rows', None)
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 
+
 def data_refresher(request):
+
     fund_codes = ["005928", "000001"]
     freq = 5
-    mult_fund_display(fund_codes, freq)
-    JsonResponse(request)
+   # mult_fund_display(fund_codes, freq)
+    return HttpResponse(request)
+
 
 def index(request):
     fund_codes = ["005928", "000001"]
     freq = 5
-    #mult_fund_display(fund_codes, freq)
-    return render(request,"homePage.html")
+    # mult_fund_display(fund_codes, freq)
+    return render(request, "homePage.html")
 
-#测试ajax
+
+# 测试ajax
 @require_http_methods(["GET", "POST"])
 def test_ajax(request):
     print("41")
@@ -41,53 +46,53 @@ def test_ajax(request):
     res["msg"] = s3
     return JsonResponse(res)
 
+
 def dataUpDate(request):
     return render(request)
 
 
-
 # 获取多个股票的原始数据字典
-def get_origin_data(freq,stock_codes):
-    origin_data_dict={}
-    data_time=str(datetime.today()).split('.')[0]
-    data_time=str(data_time).replace(":",",")
+def get_origin_data(freq, stock_codes):
+    origin_data_dict = {}
+    data_time = str(datetime.today()).split('.')[0]
+    data_time = str(data_time).replace(":", ",")
     for stock_code in stock_codes.copy():
-            # 现在的时间
-            now = str(datetime.today()).split('.')[0]
-            # 获取最新一个交易日的分钟级别股票行情数据
-            origin_data_dict[str(stock_code)]= ef.stock.get_quote_history(stock_code, klt=freq)
-            # print(f'已在 {now}, 完成股票: {stock_code} 的行情数据获取')
-    return data_time,origin_data_dict
+        # 现在的时间
+        now = str(datetime.today()).split('.')[0]
+        # 获取最新一个交易日的分钟级别股票行情数据
+        origin_data_dict[str(stock_code)] = ef.stock.get_quote_history(stock_code, klt=freq)
+        # print(f'已在 {now}, 完成股票: {stock_code} 的行情数据获取')
+    return data_time, origin_data_dict
 
-#转原始数据excel
-def trans_to_excel(data_time,dict_all):
-    excel_name="数据获取历史/行情数据获取"+data_time+".xlsx"
+
+# 转原始数据excel
+def trans_to_excel(data_time, dict_all):
+    excel_name = "数据获取历史/行情数据获取" + data_time + ".xlsx"
     writer = pd.ExcelWriter(excel_name)
-    for k,v in dict_all.items():
-            v.to_excel(writer, sheet_name=k)
+    for k, v in dict_all.items():
+        v.to_excel(writer, sheet_name=k)
     writer.close()
 
-#多个股票的原始数据字典→涨跌幅字典
-def get_core_data(origin_data_dict,stock_code):
-    single_df=origin_data_dict[stock_code]
-    single_df["日期"]=pd.to_datetime(single_df["日期"])
-    single_df_today=single_df[single_df["日期"].dt.date==datetime.today().date()]
-    single_df_last_close=single_df[single_df["日期"].dt.date!=(datetime.today().date())].iloc[-1,:,]
-    real_time_change=single_df_today[["日期","收盘"]]
-    real_time_change[stock_code]=real_time_change["收盘"]/single_df_last_close['收盘']-1
-    real_time_change=real_time_change.drop("收盘",axis=1)
-    real_time_change=real_time_change.set_index("日期")
+
+# 多个股票的原始数据字典→涨跌幅字典
+def get_core_data(origin_data_dict, stock_code):
+    single_df = origin_data_dict[stock_code]
+    single_df["日期"] = pd.to_datetime(single_df["日期"])
+    single_df_today = single_df[single_df["日期"].dt.date == datetime.today().date()]
+    single_df_last_close = single_df[single_df["日期"].dt.date != (datetime.today().date())].iloc[-1, :, ]
+    real_time_change = single_df_today[["日期", "收盘"]]
+    real_time_change[stock_code] = real_time_change["收盘"] / single_df_last_close['收盘'] - 1
+    real_time_change = real_time_change.drop("收盘", axis=1)
+    real_time_change = real_time_change.set_index("日期")
     return real_time_change
 
-#将多个股票的原始数据字典转为一个涨跌幅dataframe
+
+# 将多个股票的原始数据字典转为一个涨跌幅dataframe
 def conbin_change(origin_data_dict):
-    conbin_change=pd.DataFrame()
-    for k,v in origin_data_dict.items():
-        conbin_change=pd.concat([conbin_change,get_core_data(origin_data_dict,k)],axis=1)
+    conbin_change = pd.DataFrame()
+    for k, v in origin_data_dict.items():
+        conbin_change = pd.concat([conbin_change, get_core_data(origin_data_dict, k)], axis=1)
     return conbin_change
-
-
-
 
 
 # 根据基金编号获取
@@ -105,11 +110,10 @@ def get_fin_change_weighted(fund_code, freq):
     fin_change_weighted["加权合计"] = fin_change_weighted_temp.apply(lambda x: x.sum(), axis=1)
     return fin_change_weighted.applymap(lambda x: '%.2f%%' % (x * 100))
 
+
 # 多基金数据获取并打印
 def mult_fund_display(fund_codes, freq):
     for i in fund_codes:
         # displaying the DataFrame
         print("基金编号:" + i)
         print(tabulate(get_fin_change_weighted(i, freq).tail(10), headers='keys', tablefmt='pretty'))
-
-
