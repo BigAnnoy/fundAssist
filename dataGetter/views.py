79f4e484a4e9dbc1b636d7bb02175e5949da1b07
BigAnnoy <xm1294395667@outlook.com>
2023-09-17 21:45:53 +0800

    hello`

--- dataGetter/views.py
+++ dataGetter/views.py
@@ -23,9 +23,10 @@ from django_pandas.io import read_frame
 def data_refresher(request):
     fund_codes = ["005928", "000001"]
     freq = 5
-    fin_change_weighted=get_fin_change_weighted(fund_codes[0],freq)
+    data_refresh_time,fin_change_weighted=get_fin_change_weighted(fund_codes[0],freq)
    # mult_fund_display(fund_codes, freq)
     res = {"fundCode": fund_codes[0]}
+    res["data_refresh_time"]=data_refresh_time
     res["fundData"]=fin_change_weighted.to_html()
     print("数据已经刷新")
     return JsonResponse(res)
@@ -58,15 +59,15 @@ def dataUpDate(request):
 # 获取多个股票的原始数据字典
 def get_origin_data(freq, stock_codes):
     origin_data_dict = {}
-    data_time = str(datetime.today()).split('.')[0]
-    data_time = str(data_time).replace(":", ",")
+
+    data_refresh_time = str(datetime.today()).split('.')[0]
+    data_time = str(data_refresh_time).replace(":", ",")
     for stock_code in stock_codes.copy():
         # 现在的时间
         now = str(datetime.today()).split('.')[0]
         # 获取最新一个交易日的分钟级别股票行情数据
         origin_data_dict[str(stock_code)] = ef.stock.get_quote_history(stock_code, klt=freq)
-        # print(f'已在 {now}, 完成股票: {stock_code} 的行情数据获取')
-    return data_time, origin_data_dict
+    return data_refresh_time, origin_data_dict
 
 
 # 转原始数据excel
@@ -82,9 +83,13 @@ def trans_to_excel(data_time, dict_all):
 def get_core_data(origin_data_dict, stock_code):
     single_df = origin_data_dict[stock_code]
     single_df["日期"] = pd.to_datetime(single_df["日期"])
-    single_df_today = single_df[single_df["日期"].dt.date == datetime.today().date()]
-    single_df_last_close = single_df[single_df["日期"].dt.date != (datetime.today().date())].iloc[-1, :, ]
-    real_time_change = single_df_today[["日期", "收盘"]]
+    single_df_last=pd.DataFrame()
+    last_deal_time=datetime.today().date()
+    while len(single_df_last)==0:
+        single_df_last=single_df[single_df["日期"].dt.date == last_deal_time]
+        last_deal_time=last_deal_time-timedelta(1)
+    single_df_last_close = single_df[single_df["日期"].dt.date != last_deal_time].iloc[-1, :, ]
+    real_time_change = single_df_last[["日期", "收盘"]]
     real_time_change[stock_code] = real_time_change["收盘"] / single_df_last_close['收盘'] - 1
     real_time_change = real_time_change.drop("收盘", axis=1)
     real_time_change = real_time_change.set_index("日期")
@@ -105,14 +110,14 @@ def get_fin_change_weighted(fund_code, freq):
     stock_codes = fund_cons["股票简称"].tolist()
     stock_perc = fund_cons["持仓占比"]
     while True:
-        data_time, origin_data_dict = get_origin_data(freq, stock_codes)
+        data_refresh_time, origin_data_dict = get_origin_data(freq, stock_codes)
         fin_change = conbin_change(origin_data_dict)
         break
     e = fund_cons["持仓占比"].sum()
     fin_change_weighted = fin_change
     fin_change_weighted_temp = fin_change * fund_cons.set_index("股票简称")["持仓占比"] / e
     fin_change_weighted["加权合计"] = fin_change_weighted_temp.apply(lambda x: x.sum(), axis=1)
-    return fin_change_weighted.applymap(lambda x: '%.2f%%' % (x * 100))
+    return data_refresh_time,fin_change_weighted.applymap(lambda x: '%.2f%%' % (x * 100))
 
 
 # 多基金数据获取并打印
@@ -120,7 +125,7 @@ def mult_fund_display(fund_codes, freq):
     for i in fund_codes:
         # displaying the DataFrame
         print("基金编号:" + i)
-        print(tabulate(get_fin_change_weighted(i, freq).tail(10), headers='keys', tablefmt='pretty'))
+        print(tabulate(i,get_fin_change_weighted(i, freq).tail(10), headers='keys', tablefmt='pretty'))
 
 def table_view(request):
     data = {'name': ['Alice', 'Bob', 'Charlie', 'Dave'],
