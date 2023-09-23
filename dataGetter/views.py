cbccc3baee5a03d00b2a6e6a62278bffa8f7446d
BIGANNOY <xm1294395667@outlook.com>
2023-09-23 19:37:21 +0800

    0.2

--- dataGetter/views.py
+++ dataGetter/views.py
@@ -41,7 +41,7 @@ def data_refresher(request):
     if (datetime.today()-timedelta(seconds=60)>data_refresh_last_time) or res["fund_codes"].sort()!=data_refresh_last_list:
         print("数据抓取开始"+str(datetime.today()))
         for i in res["fund_codes"]:
-            r,res["fund_data"][i]=get_fin_change_weighted(i,freq,begin_time)
+            res["fund_data"][i]=get_fin_change_weighted(i,freq,begin_time)
             res["fund_data"][i]=res["fund_data"][i].to_html(classes="table-light")
         data_refresh_latest=res["fund_data"]
         data_refresh_last_time=datetime.today()
@@ -64,9 +64,8 @@ def index(request):
 
 # 获取多个股票的原始数据字典
 def get_origin_data(freq, stock_codes,begin_time):
-    data_refresh_time = str(datetime.today()).split('.')[0]
     origin_data_dict= ef.stock.get_quote_history(stock_codes, klt=freq,beg=begin_time)
-    return data_refresh_time, origin_data_dict
+    return  origin_data_dict
 
 
 # 转原始数据excel
@@ -78,17 +77,15 @@ def trans_to_excel(data_time, dict_all):
     writer.close()
 
 
-# 多个股票的原始数据字典→涨跌幅字典
-def get_core_data(origin_data_dict, stock_code):
-    single_df = origin_data_dict[stock_code]
-    single_df["日期"] = pd.to_datetime(single_df["日期"])
+# 多个股票的原始数据字典→涨跌幅
+def get_core_data(single_df, stock_code):
+    single_df["日期"]=pd.to_datetime(single_df["日期"])
     single_df_last=pd.DataFrame()
     last_deal_time=datetime.today().date()
     while len(single_df_last)==0:
         single_df_last=single_df[single_df["日期"].dt.date == last_deal_time]
         last_deal_time=last_deal_time-timedelta(1)
     single_df_last_close = single_df[single_df["日期"].dt.date != (last_deal_time+timedelta(1))].iloc[-1, :, ]
-
     real_time_change = single_df_last[["日期", "收盘"]]
     real_time_change[stock_code] = real_time_change["收盘"] / single_df_last_close['收盘'] - 1
     real_time_change = real_time_change.drop("收盘", axis=1)
@@ -96,38 +93,29 @@ def get_core_data(origin_data_dict, stock_code):
     return real_time_change
 
 
-# 将多个股票的原始数据字典转为一个涨跌幅dataframe
+# 将多个股票的原始数据字典，计算涨跌幅
 def conbin_change(origin_data_dict):
     conbin_change = pd.DataFrame()
     for k, v in origin_data_dict.items():
-        conbin_change = pd.concat([conbin_change, get_core_data(origin_data_dict, k)], axis=1)
+        conbin_change = pd.concat([conbin_change, get_core_data(v, k)], axis=1)
     return conbin_change
 
 
-# 根据基金编号获取
+# 根据单个基金编号获取
 def get_fin_change_weighted(fund_code, freq, begin_time):
+    #获取持仓占比
     fund_cons = ef.fund.get_invest_position(fund_code)
     stock_codes = fund_cons["股票简称"].tolist()
-    stock_perc = fund_cons["持仓占比"]
-    while True:
-        data_refresh_time, origin_data_dict = get_origin_data(freq, stock_codes,begin_time)
-        fin_change = conbin_change(origin_data_dict)
-        break
-    fin_change_weighted = fin_change.copy()
+    #获取基金下，多个股票的原始数据
+    origin_data_dict = get_origin_data(freq, stock_codes,begin_time)
+    #将原始数据转化为涨幅df
+    fin_change_weighted = conbin_change(origin_data_dict)
+    fin_change_weighted_temp = fin_change_weighted * fund_cons.set_index("股票简称")["持仓占比"]
     col_temp=fin_change_weighted.columns.tolist()
     col_temp.insert(0,"加权合计")
     fin_change_weighted=fin_change_weighted.reindex(columns=col_temp)
-    fin_change_weighted_temp = fin_change * fund_cons.set_index("股票简称")["持仓占比"]
     fin_change_weighted["加权合计"] = fin_change_weighted_temp.apply(lambda x: x.sum(), axis=1)/100
-    return data_refresh_time,fin_change_weighted.applymap(lambda x: '%.2f%%' % (x * 100))
-
-
-# 多基金数据获取并打印
-def mult_fund_display(fund_codes, freq):
-    for i in fund_codes:
-        # displaying the DataFrame
-        print("基金编号:" + i)
-        print(tabulate(i,get_fin_change_weighted(i, freq).tail(10), headers='keys', tablefmt='pretty'))
+    return fin_change_weighted.applymap(lambda x: '%.2f%%' % (x * 100))
 
 def add_fund(request):
     fund_code=request.POST.get("fundcode")
